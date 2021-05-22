b2e95bc421330a7f3cbf7aa21736dc0eaf8339c2
bogdau <74491913+bogdau@users.noreply.github.com>
2021-05-22 14:53:29 +0300

    Add files via upload

--- Shop_center/main.cpp
+++ Shop_center/main.cpp
@@ -1,48 +1,51 @@
-
 #include <iostream>
 #include <unistd.h>  //using for sleep
 
 #include "shops.h"
 
-void choiseShopMenu(Shops& shops, People& people) {
+void choiseShopMenu(Buying& buying, People& people)
+{
+    FoodClothAmusent allList;
     std::cout << "\n1. Grosery store\n2. Cloth store\n3. Amusent store " << std::endl;
     char choiseShop = 0;
     std::cin >> choiseShop;
     switch (choiseShop) {
     case '1':
-        shops.groseryShops(people);
+        allList.foodprint();
+        buying.foodbuy(people, allList);
         break;
     case '2':
+        allList.clothprint();
+        buying.clotbuy(people, allList);
         break;
     case '3':
+        allList.amusentprint();
+        buying.amusetbuy(people, allList);
         break;
     }
 }
 
 int main()
 {
-    Shops shops;
+    Buying buying;
     People people;
 
     bool stop = false;
     while (stop != true) {
         std::cout << "\tMAIN MENU" << std::endl;
-        std::cout << "1. List of shops\n2. Inventory\n3. Go to shop?\n4. Exit" << std::endl;
+        std::cout << "\n1. Inventory\n2. Go to shop?\n3. Exit" << std::endl;
         std::cout << "What you want: _\b_\b";
         char choise;
         std::cin >> choise;
 
         switch (choise) {
         case '1':
-            shops.listOfShops();
-            break;
-        case '2':
             people.print();
             break;
-        case '3':
-            choiseShopMenu(shops, people);
+        case '2':
+            choiseShopMenu(buying, people);
             break;
-        case '4':
+        case '3':
             stop = true;
             break;
         default:
